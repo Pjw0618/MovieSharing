9849a14e677821b9b8918f50c9173e10ba473a86
Jiawen Peng <jPeng@Jiawens-MacBook-Air.local>
2017-12-11 14:29:04 -0500

    searching + login input data transfer

--- FrontEnd/src/components/Searching.jsx
+++ FrontEnd/src/components/Searching.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 import '../style/css/Searching.css';
 
 class Searching extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {searchKey: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      //got the key, waiting for the searching api
+      handleChange(event) {
+        this.setState({searchKey: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('A key was submitted: ' + this.state.searchKey);
+        event.preventDefault();
+      }
     render() {
         return (            
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                    <form action="#" method="#" role="search">
+                    <form onSubmit={this.handleSubmit} role="search">
                         <div className="input-group">
-                        <input className="form-control" placeholder="" name="srch-term" id="ed-srch-term" type="text" />
+                        <input className="form-control" name="srch-term" id="ed-srch-term" type="text" value={this.state.searchKey} onChange={this.handleChange} />
                         <div className="input-group-btn">
                             <button type="submit" id="searchbtn">search</button>
                         </div>
