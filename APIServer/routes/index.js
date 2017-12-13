3c8c014416cc8fe3d127e6c1fa5a05f8d6c939d0
boling77 <ydai4@stevens.edu>
2017-12-13 10:28:21 -0500

    index change

--- APIServer/routes/index.js
+++ APIServer/routes/index.js
@@ -4,6 +4,8 @@ const comment = require("./comment");
 const constructorMethod = (app) => {
     app.use("/sharemessage", sharemessage);
     app.use("/comment", comment);
+    app.use("/user", user);
+    app.use("/movie", movie);
 
     app.use("*", (req, res) => {
         res.status(404).json({error: "Not found"});
