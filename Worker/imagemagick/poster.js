032746cf9caa95bc62b18aca1590924bc2b5b1c4
Yaoqi <yqd@outlook.com>
2017-12-14 14:57:58 -0500

    bug fixes

--- Worker/imagemagick/poster.js
+++ Worker/imagemagick/poster.js
@@ -1,9 +1,11 @@
 // this module process the poster picture user uploaded to a standard foramt 
 // take the url of original picture as input and return the fixed picture's url so the data.movies can call it easily
 const im = require('imagemagick');
+const uuid = require('node-uuid');
 
 let exportedMethods = {
     processPoster(url, id){
+        console.log(url)
         const desPath = "../../FrontEnd/public/processedposters/";
         var optionsObj = {
             srcPath: url,
@@ -21,6 +23,20 @@ let exportedMethods = {
         };
         im.resize(optionsObj, function (err, stdout) {
             if (err) throw "convert poster failed";
+        });
+        return optionsObj.dstPath;
+    },
+
+    precessScreen(url){
+        const desPath = "../../FrontEnd/public/processedscreens/";
+        var optionsObj = {
+            srcPath: url,
+            dstPath: desPath + uuid.v4 + ".png",
+            quality: 1.0,
+            format: 'png',
+        };
+        im.resize(optionsObj, function (err, stdout) {
+            if (err) throw "convert screen failed";
         });
         return optionsObj.dstPath;
     }
