98615e8162a256f34fa4410fd5585d6019cbef16
Jiawen Peng <jPeng@Jiawens-Air.fios-router.home>
2017-12-16 11:58:12 -0500

    watched + wish list

--- FrontEnd/src/components/UserLists.jsx
+++ FrontEnd/src/components/UserLists.jsx
@@ -5,19 +5,19 @@ import Wish from './WishList';
 class UserLists extends React.Component {
     render() {
         return (
-            <div class="panel with-nav-tabs panel-default">
-            <div class="panel-heading">
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a href="#tab1default" data-toggle="tab">Wish List</a></li>
+            <div className="panel with-nav-tabs panel-default">
+            <div className="panel-heading">
+                    <ul className="nav nav-tabs">
+                        <li className="active"><a href="#tab1default" data-toggle="tab">Wish List</a></li>
                         <li><a href="#tab2default" data-toggle="tab">Watched List</a></li>
                     </ul>
             </div>
-            <div class="panel-body">
-                <div class="tab-content">
-                    <div class="tab-pane fade in active" id="tab1default">
+            <div className="panel-body">
+                <div className="tab-content">
+                    <div className="tab-pane fade in active" id="tab1default">
                         <Wish />
                     </div>
-                    <div class="tab-pane fade" id="tab2default">
+                    <div className="tab-pane fade" id="tab2default">
                         <Watched />
                     </div>
                 </div>
