a7fd8ab587c0b27a3ba1ff8370bca6ad52b49c75
Jiawen Peng <jPeng@Jiawens-Air.fios-router.home>
2017-12-16 01:14:09 -0500

    temp save

--- FrontEnd/src/components/WishList.jsx
+++ FrontEnd/src/components/WishList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+var FormData = require('form-data');
 
 class Wish extends React.Component {
     constructor(props) {
@@ -9,63 +10,62 @@ class Wish extends React.Component {
         }
     }
 
-    // componentDidMount() {
-    //     fetch('http://localhost:3001/user/getUserByDbId/'+this.state.userId)
-    //     .then((response) => {
-    //         console.log(response);
-    //         return response.json();
-    //     })
-    //     .catch((e) => {
-    //         console.log(e);
-    //     })
-    //     .then((message) => {
-    //         message.wishList.forEach(element => {
-    //             console.log(element);
-    //             fetch('http://localhost:3001/movie/getMovieById/'+element)
-    //             .then((res) => {
-    //                 return res.json();
-    //             })
-    //             .catch((e) => {
-    //                 console.log(e);
-    //             })
-    //             .then((result) => {
-    //                 this.state.wishList.push(result);
-    //             })
-    //         });
-    //     })
-    //     console.log(this.state.wishList)
-    // }
-
     componentDidMount() {
-        fetch('http://localhost:3001/user/getUserByDbId/' + this.state.userId)
+        fetch('http://localhost:3001/user/getUserByDbId/'+this.state.userId)
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+        .then((message) => {
+            fetch('http://localhost:3001/movie/getMoviesByIdList/'+JSON.stringify(message.wishList))
             .then((response) => {
-                console.log(response);
                 return response.json();
             })
             .catch((e) => {
                 console.log(e);
             })
             .then((message) => {
-    
-                let promises = [];
-                message.wishList.forEach(element => {
-                    promises.push(fetch('http://localhost:3001/movie/getMovieById/' + element))
-                });
-                Promise.all(promises).then((responses) => {
-                    Promise.all(responses).then((values) => {
-                        values.forEach((result)=>{
-                            this.state.wishList.push(result);
-                        })
-                    })
+                this.setState({
+                    wishList: message
                 })
             })
-            console.log(this.state.wishList.length)
-        }    
+        })
+        console.log(this.state.wishList)
+    }
+
+    // componentDidMount() {
+    //     fetch('http://localhost:3001/user/getUserByDbId/' + this.state.userId)
+    //         .then((response) => {
+    //             console.log(response);
+    //             return response.json();
+    //         })
+    //         .catch((e) => {
+    //             console.log(e);
+    //         })
+    //         .then((message) => {
+    
+    //             let promises = [];
+    //             message.wishList.forEach(element => {
+    //                 promises.push(fetch('http://localhost:3001/movie/getMovieById/' + element))
+    //             });
+    //             Promise.all(promises).then((responses) => {
+    //                 Promise.all(responses).then((values) => {
+    //                     values.forEach((result)=>{
+    //                         this.state.wishList.push(result);
+    //                     })
+    //                 })
+    //             })
+    //         })
+    //         console.log(this.state.wishList.length)
+    //     }    
 
     render() {
         return (
             <section className="p-0" id="WishList">
-                <div className="container-fluid p-0">
+                {/* <div className="container-fluid p-0">
                     <div className="row no-gutters popup-gallery">
                         <h2 className="namestyle">Wish List</h2>
                         <div className="card-group">
@@ -77,7 +77,7 @@ class Wish extends React.Component {
                             </ul>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </section>
         )
     }
